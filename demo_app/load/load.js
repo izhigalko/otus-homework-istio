04f6abceffde91c5f1d3f0ce8ae4e8d156059323
dgrunskiy <dgrunskiy@sdvor.com>
2021-09-12 16:55:51 +0500

    трафик через istio-ingressgateway

--- demo_app/load/load.js
+++ demo_app/load/load.js
@@ -11,8 +11,15 @@ export let options = {
         { duration: '1m', target: 0 },
     ]
 };
-
+/*
+export INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].nodePort}')
+export SECURE_INGRESS_PORT=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].nodePort}')
+export INGRESS_HOST=$(minikube ip)
+export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
+echo "http://$GATEWAY_URL/"
+результат укзать ниже
+ */
 export default () => {
-    http.get(`http://homework.apps`);
+    http.get(`http://192.168.49.2:31414/`);
     sleep(1)
 };
