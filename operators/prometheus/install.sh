f045bd5106beb1ac28e44d3c31b5dc70972fc446
Anton Sidelnikov <ndmeredian@gmail.com>
2022-02-02 16:01:13 +0700

    Install istio

--- operators/prometheus/install.sh
+++ operators/prometheus/install.sh
@@ -11,7 +11,7 @@ helm repo update
 IS_INSTALLED="$(helm list -n monitoring | grep prometheus)"
 if [[ -n "$IS_INSTALLED" ]]; then
 	echo 'Upgraging prometheus release with helm';
-	helm upgrade -n monitoring --version "13.7.2" -f ./operator-values.yaml prometheus prometheus-community/kube-prometheus-stack
+	helm upgrade -n monitoring --version "21.0.0" -f ./operator-values.yaml prometheus prometheus-community/kube-prometheus-stack
 else \
 	echo 'Installing prometheus release with helm';
 	helm install -n monitoring --version "21.0.0" -f ./operator-values.yaml prometheus prometheus-community/kube-prometheus-stack
